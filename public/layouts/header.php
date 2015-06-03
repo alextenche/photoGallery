4106bdd15be65df59cca51234156f182855bad76
Alex <alex.tenche@gmail.com>
2015-06-03 18:31:41 +0300

    masonry not working with slow loading pictures
    mabye have some small pictures for thumbnails

--- public/layouts/header.php
+++ public/layouts/header.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/css/main.css">
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
 </head>
 
 <body>
